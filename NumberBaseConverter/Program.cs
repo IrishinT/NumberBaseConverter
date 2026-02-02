b08a338d98603d6c00cd880846bd04fd35b4f856
Timofey Irishin <t.irishin@inbox.ru>
2026-02-02 21:46:59 +0300

    Удалил тестовое сообщение

--- NumberBaseConverter/Program.cs
+++ NumberBaseConverter/Program.cs
@@ -12,7 +12,6 @@ namespace NumberBaseConverter
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-            Console.Write("Test123456");
         }
     }
 }
