ca1b27be4622201a615d1c2be219698631d4e584
Timofey Irishin <t.irishin@inbox.ru>
2026-02-05 21:44:35 +0300

    Реализовал валидацию входных данных. - Проверку на пустые строки или нулевые значения. - Отлов ошибок парсинга чисел
    Реализовал конвертацию всех входных данных в целочисленные типы

--- NumberBaseConverter/Form1.cs
+++ NumberBaseConverter/Form1.cs
@@ -47,6 +47,51 @@ namespace NumberBaseConverter
                 MessageBox.Show("Выберите системы счисления", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+
+            if(!int.TryParse(fromComboBox.SelectedItem.ToString(), out int inputBase))
+            {
+                MessageBox.Show("""
+                    Произошла внутренняя ошибка в ходе выполнения данной операции!
+                    Ошибка перевода входной СС в число.
+                    """, 
+                    "Критическая ошибка!", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Stop);
+                return;
+            }
+
+            if (!int.TryParse(fromComboBox.SelectedItem.ToString(), out int outBase))
+            {
+                MessageBox.Show("""
+                    Произошла внутренняя ошибка в ходе выполнения данной операции!
+                    Ошибка перевода выходной СС в число.
+                    """,
+                    "Критическая ошибка!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Stop);
+                return;
+            }
+
+            int inputNumber;
+
+            try
+            {
+                inputNumber = Convert.ToInt32(input, inputBase);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Входное значение не является числом для заданной системы счисления", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Входное значение слишком большое", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // TODO: вызов метода из библиотеки классов для выполнения бизнес-логики с параметрами inputNumber, inputBase, outputBase
+
+
         }
 
 
